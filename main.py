c1fc225d8fd7c378999a1cc4b281dc2d947d8c09
hind <bennounehind@gmail.com>
2025-12-29 18:29:58 +0100

    mon pdfblumber

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hiiiindbennounnew")
+from fastapi import FastAPI, UploadFile, File
+import pdfplumber
+
+app = FastAPI()
+
+@app.post("/upload-pdf/")
+async def upload_pdf(file: UploadFile = File(...)):
+    if not file.filename.endswith(".pdf"):
+        return {"error": "Le fichier doit être un PDF."}
+
+    content = ""
+    with pdfplumber.open(file.file) as pdf:
+        for page in pdf.pages:
+            text = page.extract_text()
+            if text:
+                content += text + "\n"
+
+    return {"filename": file.filename, "text": content}
